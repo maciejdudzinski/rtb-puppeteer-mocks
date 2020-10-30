bf008bb1178ce5b1c386a2282f14453b5af8cfc8
Maciej Dudzinski <maciej@tagvenue.com>
2020-10-30 21:23:59 +0100

    Make configurable

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
+const yargs = require('yargs')
 const { sleep } = require('./util')
 const { ViewPage } = require('./pom/ViewPage')
 
-const path = `/play/${process.argv[2]}`
+const ID = yargs.argv.id;
+const TABS = yargs.argv.n;
 
-const createTab = async () => {
-  const tab = await new ViewPage(path).launch()
+const PATH = `/play/${ID}`
+
+console.log(`Starting on: ${PATH} with ${TABS} tabs`);
+
+const createTab = async (id) => {
+  const tab = await new ViewPage(PATH).launch()
 
   await sleep(1000)
   await tab.toggleVideo()
   await sleep(1000)
-  await tab.enterName('Al')
+  await tab.enterName(`Alfredo #${id}`)
   await tab.joinViewing()
   await tab.turnOffAudio()
 }
 
 ;(async function() {
-  await Promise.all([createTab(), createTab(), createTab()])
-  await sleep(1000000)
+  await Promise.all((Array.from(new Array(TABS))).map((el, i) => createTab(i)))
+  console.log('Waiting to close...');
+  await sleep(20 * 60 * 1000)
 })()
