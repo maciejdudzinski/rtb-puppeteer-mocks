1e9c48184bc899e0593ff2664e1909fd25874240
Maciej Dudzinski <maciej@tagvenue.com>
2020-10-30 22:08:14 +0100

    Install EC2

---
+++ install.sh
@@ -0,0 +1,12 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+mkdir app
+cd app
+sudo yum install git
+git clone https://github.com/maciejdudzinski/rtb-puppeteer-mocks .
+npm i
+sudo yum install cups-libs dbus-glib libXrandr libXcursor libXinerama cairo cairo-gobject pango
+sudo yum install pango.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXtst.x86_64 cups-libs.x86_64 libXScrnSaver.x86_64 libXrandr.x86_64 GConf2.x86_64 alsa-lib.x86_64 atk.x86_64 gtk3.x86_64 ipa-gothic-fonts xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc -y
+
+TARGET_DOMAIN=play.tagvenue.uk node index.js --id fpZLc1Ixt9sJYtNWWl1V -n 5
