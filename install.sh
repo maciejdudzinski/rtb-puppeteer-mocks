a54228184e4cd8235a819f9fbdc28dad123e743a
Maciej Dudzinski <maciej@tagvenue.com>
2020-11-02 00:41:58 +0100

    Better silence

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ npm i
 sudo yum install cups-libs dbus-glib libXrandr libXcursor libXinerama cairo cairo-gobject pango -y
 sudo yum install pango.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXtst.x86_64 cups-libs.x86_64 libXScrnSaver.x86_64 libXrandr.x86_64 GConf2.x86_64 alsa-lib.x86_64 atk.x86_64 gtk3.x86_64 ipa-gothic-fonts xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc -y
 
-node index.js play.tagvenue.uk/play/fpZLc1Ixt9sJYtNWWl1V -n 5
+node index.js https://play.tagvenue.uk/play/ohzxCMcq19pRJxOm0c8p -n 5 --delay 500 --no-audio
